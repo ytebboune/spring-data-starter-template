190f534a8f8e172265fae3cc5a7052e4595ba341
YacineTebboune <yacine.tebboune@alithya.com>
2021-12-06 11:58:52 +0100

    Base Application

---
+++ src/main/java/com/commonstarter/BaseApplication.java
@@ -0,0 +1,16 @@
+package com.commonstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseApplication {
+
+	public static void main(String[] args) {
+		/**
+		 * Application entry point
+		 */
+		SpringApplication.run(BaseApplication.class);
+	}
+	
+}
